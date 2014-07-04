1ef5035b97319ec118279032962cafd4948c869d
Ben Hartman <imachuchu@gmail.com>
2014-07-03 20:54:12 -0700

    Added in command to remove test data

--- tasks.py
+++ tasks.py
@@ -1,19 +1,28 @@
 from invoke import run, task
 
+testFiles = [
+    {'path': "test1.txt", 'content': "Test 1's content"},
+    {'path': "test2.txt", 'content': "Test 2's content"}
+]
+
+
 @task
 def addTestData():
     """Adds the test data to the couchdb database"""
     from dvfs.couchdb.dbFile import dbFile
     import os
-    files = [
-        {'name': "test1.txt", 'content': "Test 1's content"},
-        {'name': "test2.txt", 'content': "Test 2's content"}
-    ]
     os.chdir("dvfs/base")
-    for instance in files:
+    for instance in testFiles:
         file = open(instance['name'], 'w+')
         file.write(instance['content'])
         file.close()
-
-
     print("Adding test data")
+
+@task
+def delTestData():
+    """Deletes test data from the database"""
+    import os
+    os.chdir("dvfs/base")
+    for instance in testFiles:
+        os.remove(instance['path'])
+    print("Deleting test data")
