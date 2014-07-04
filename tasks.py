e701064999c3979dd1a00d81aa4f3d27c0d4e394
Ben Hartman <imachuchu@gmail.com>
2014-07-03 21:07:28 -0700

    Added couchdb insertions for adding test data

--- tasks.py
+++ tasks.py
@@ -4,18 +4,32 @@ testFiles = [
     {'path': "test1.txt", 'content': "Test 1's content"},
     {'path': "test2.txt", 'content': "Test 2's content"}
 ]
-
+dbName = 'dvfs'
 
 @task
 def addTestData():
     """Adds the test data to the couchdb database"""
     from dvfs.couchdb.dbFile import dbFile
+    import couchdbkit as ck #For an ORM'ish interface to couchDB
     import os
+    from datetime import datetime
+    server = ck.Server()
+    database = server.get_or_create_db(dbName)
     os.chdir("dvfs/base")
     for instance in testFiles:
-        file = open(instance['name'], 'w+')
+        """File system parts"""
+        file = open(instance['path'], 'w+')
         file.write(instance['content'])
         file.close()
+
+        """Couchdb parts"""
+        newFile = dbFile()
+        newFile.set_db(database)
+        newFile.path = instance['path']
+        newFile.createTime = datetime.utcnow()
+        newFile.accessTime = newFile.createTime
+        newFile.modifyTime = newFile.createTime
+        newFile.save()
     print("Adding test data")
 
 @task
