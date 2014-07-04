2688f173239c889eaaaa4e3cbf88ab5853ca67ec
Ben Hartman <imachuchu@gmail.com>
2014-07-03 20:36:54 -0700

    Invoke can add the test files

--- tasks.py
+++ tasks.py
@@ -1,7 +1,19 @@
 from invoke import run, task
 
-
 @task
 def addTestData():
     """Adds the test data to the couchdb database"""
+    from dvfs.couchdb.dbFile import dbFile
+    import os
+    files = [
+        {'name': "test1.txt", 'content': "Test 1's content"},
+        {'name': "test2.txt", 'content': "Test 2's content"}
+    ]
+    os.chdir("dvfs/base")
+    for instance in files:
+        file = open(instance['name'], 'w+')
+        file.write(instance['content'])
+        file.close()
+
+
     print("Adding test data")
