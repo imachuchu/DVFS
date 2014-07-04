23b71c1c0c1a1ead7cf7dc3888655a47b1f2ccc7
Ben Hartman <imachuchu@gmail.com>
2014-07-03 21:23:46 -0700

    Added more components to the add test date task

--- tasks.py
+++ tasks.py
@@ -9,10 +9,11 @@ dbName = 'dvfs'
 @task
 def addTestData():
     """Adds the test data to the couchdb database"""
-    from dvfs.couchdb.dbFile import dbFile
     import couchdbkit as ck #For an ORM'ish interface to couchDB
     import os
     from datetime import datetime
+    from hashlib import sha1
+    from dvfs.couchdb.dbFile import dbFile
     server = ck.Server()
     database = server.get_or_create_db(dbName)
     os.chdir("dvfs/base")
@@ -29,6 +30,9 @@ def addTestData():
         newFile.createTime = datetime.utcnow()
         newFile.accessTime = newFile.createTime
         newFile.modifyTime = newFile.createTime
+        newFile.fileHash = sha1(instance['content']).hexdigest()
+        newFile.mode = "testMode" #almost assuredly wrong
+        newFile.size = os.stat(instance['path']).st_size
         newFile.save()
     print("Adding test data")
 
