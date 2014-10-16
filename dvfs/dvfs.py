2e697771dae43c28b1b2da6ddba8f9202f3df0fb
Ben Hartman <imachuchu@gmail.com>
2014-10-15 23:37:12 -0700

    Sort of a clean sweep reimplementation

--- dvfs/dvfs.py
+++ dvfs/dvfs.py
@@ -1,286 +1,136 @@
 #!/usr/bin/env python
-from fuse import FUSE, FuseOSError, Operations, LoggingMixIn
-import fuse
+
+import logging
+
+from collections import defaultdict
+from errno import ENOENT
 from stat import S_IFDIR, S_IFLNK, S_IFREG
-import argparse #For easy parsing of the command line arguments
-import logging #For debug/error logging
-import couchdbkit as ck
-import os
-from datetime import datetime
-from couchdb.dbObject import dbObject
-from couchdb.dbFile import dbFile
-from couchdb.dbFolder import dbFolder
+from sys import argv, exit
+from time import time
 
+from fuse import FUSE, FuseOSError, Operations, LoggingMixIn
 
-""" If we were using the old python fuse bindings
-class rootInfo(fuse.Stat):
+if not hasattr(__builtins__, 'bytes'):
+    bytes = str
+
+class Memory(LoggingMixIn, Operations):
+    'Example memory filesystem. Supports only one level of files.'
+
     def __init__(self):
-        fuse.Stat.__init__(self)
-        self.st_mode = stat.S_IFDIR | 0755
-        self.st_nlink = 2
+        self.files = {}
+        self.data = defaultdict(bytes)
+        self.fd = 0
+        now = time()
+        self.files['/'] = dict(st_mode=(S_IFDIR | 0755), st_ctime=now,
+                               st_mtime=now, st_atime=now, st_nlink=2)
 
-class folderInfo(fuse.Stat):
-    def __init__(self):
-        fuse.Stat.__init__(self)
-        self.st_mode = stat.S_IFDIR | 0755
-        self.st_nlink = 2
+    def chmod(self, path, mode):
+        self.files[path]['st_mode'] &= 0770000
+        self.files[path]['st_mode'] |= mode
+        return 0
 
-class fileInfo(fuse.Stat):
-    def __init__(self):
-        fuse.Stat.__init__(self)
-        self.st_mode = stat.S_IFREG | 0644
-        self.st_nlink = 1
-        self.st_size = 4096
-"""
+    def chown(self, path, uid, gid):
+        self.files[path]['st_uid'] = uid
+        self.files[path]['st_gid'] = gid
 
-class dvfs(LoggingMixIn, Operations):
-    def __init__(self, base, debug):
-        self.debug = debug
-        if self.debug == True:
-            logging.debug("init, base = %s" % base)
-        if base[0] == '/':
-            self.base = base
-        else:
-            self.base = os.path.dirname(os.path.realpath(__file__)) + "/" + base
-        if self.debug == True:
-            logging.debug("init done, self.base = %s" % self.base)
-        self.connectDatabase(dbName='dvfs')
+    def create(self, path, mode):
+        self.files[path] = dict(st_mode=(S_IFREG | mode), st_nlink=1,
+                                st_size=0, st_ctime=time(), st_mtime=time(),
+                                st_atime=time())
 
-    def connectDatabase(self,dbName):
-        """Sets up the database to be interacted"""
-        if self.debug == True:
-            logging.debug("connecting to database")
-        server = ck.Server()
-        self.database = server.get_or_create_db(dbName)
-        self.dataOb = dbObject.set_db(server.get_or_create_db(dbName))
+        self.fd += 1
+        return self.fd
 
     def getattr(self, path, fh=None):
-        """Returns the attributes of the specific file"""
-        if self.debug == True:
-            logging.debug("Inside the getattr function, path= %s" % path)
-
-        # All of this is debug stuff
-        from time import time
-        import errno
-        now = time()
-        st = fuse.c_stat()
-        logging.debug(st)
-        return st
-        """
-        if path == '/':
-            return dict(st_mode=(S_IFDIR | 0755), st_ctime=now, st_mtime=now, st_atime=now, st_nlink=2)
-        return dict(st_mode=(S_IFDIR | 0444), st_ctime=now, st_mtime=now, st_atime=now, st_size=4096)
-        """
-        """
-        st.st_mode = stat.S_IFDIR | 0755
-        st.st_nlink = 2
-        st.st_atime = int(time.time())
-        st.st_mtime = st.st_atime
-        st.st_ctime = st.st_atime
-        if path != '/':
-            st.st_mode = stat.S_IFDIR | 0644
-            st.st_nlink = 1
-            st.st_size = 4096
-        return st
-        """
-        # End debug stuff
-        """
-        if path == '/':
-            logging.debug("found the root")
-            now = datetime.now()
-            return dict(st_mode=(S_IFDIR | 0755), st_ctime=now, st_mtime=now, st_atime=now, st_nlink=2)
-        logging.debug("outside the root")
-        return dict(st_mode=(S_IFDIR | 0755), st_ctime=now, st_mtime=now, st_atime=now)
-        dbView = dbFile(self.dataOb)
-        fileInfo = dbView.view('dvfs/path-all', key=path).one() #There should only ever be one anyway
-        if fileInfo is None:
+        if path not in self.files:
             raise FuseOSError(ENOENT)
-        else:
-            return fileInfo.getAttributes()
-        """
 
-    def readdir(self, path, fh=None):
-        """Returns the attributes of a folder"""
-        if self.debug == True:
-            logging.debug("Inside the readdir function, path= %s" % path)
-        return fuse.c_stat()
-        return dict(st_mode=(S_IFDIR | 0755), st_ctime=now, st_mtime=now, st_atime=now, st_nlink=2)
-        direntry = ['.', '..'] 
-        dbView = dbFile(self.dataOb)
-        fileList = dbView.view('dvfs/folderPath', key=path)
-        for entry in fileList:
-            direntry.append(entry.path[len(path):].encode('utf-8')) #A bit ugly, but will always work by definition
-        return direntry
+        return self.files[path]
 
-    def read(self, path, size, offset, fh=None):
-        """Loads data from an opened file"""
-        if self.debug == True:
-            logging.debug("the read path is= " + path)
-        return ''
+    def getxattr(self, path, name, position=0):
+        attrs = self.files[path].get('attrs', {})
 
-    def open(self, path, flags=None):
-        """For checking permission of pending file
-        Needs to be implemented to pull down files that we don't have"""
-        if self.debug == True:
-            logging.debug("the open path is= " + path)
-        return -errno.EACCES
+        try:
+            return attrs[name]
+        except KeyError:
+            return ''       # Should return ENOATTR
 
-    def write(self, path, data, offset, fh=None):
-        #Update the database metadata
-        """Actually writes data into the file"""
-        if self.debug == True:
-            logging.debug("In the write call")
-        return 0
+    def listxattr(self, path):
+        attrs = self.files[path].get('attrs', {})
+        return attrs.keys()
 
-# These features aren't implemented yet, and some may never be
-    '''
-    def readlink(self, path):
-        """Reads a symbolic link
-        May be implemented
-        """
-        if self.debug == True:
-            logging.debug("In readlink, path= %s" % path)
-        pass
-    '''
-
-    '''
-    def unlink(self, path):
-        """Removes a file"""
-        if self.debug == True:
-            logging.debug("In unlink, path = %s" % path)
-        pass
-    '''
-
-    '''
-    def rmdir(self, path):
-        """Removes a directory
-        Will need to be implemented
-        """
-        if self.debug == True:
-            logging.debug("In rmdir, path = %s" % path)
-        pass
-    '''
-
-    '''
-    def symlink(self, path, path1):
-        """Creates a symbolic path
-        May be implemented
-        """
-        if self.debug == True:
-            logging.debug("In symlink, path = %s" % path)
-        pass
-    '''
-
-    '''
-    def link(self, path, path1):
-        """Creates a hard link
-        May be implemented
-        """
-        if self.debug == True:
-            logging.debug("In link, path = %s" % path)
-        pass
-    '''
-
-    '''
-    def chmod(self, path, mode):
-        """Changes the permissions on a file
-        Will need to be implemented, and will be complex
-        """
-        if self.debug == True:
-            logging.debug("In chmod, path = %s" % path)
-        return 0
-    '''
-
-    '''
-    def chown(self, path, mode):
-        """Changes the owner/group of a file
-        Will need to be implemented
-        """
-        if self.debug == True:
-            logging.debug("In chown, path = %s" % path)
-    '''
-
-    '''
-    def truncate(self, path, length, fh=None):
-        """Changes the size of a file
-        Should be easy to implement
-        """
-        if self.debug == True:
-            logging.debug("In truncate, path = %s" % path)
-        pass
-    '''
-
-    '''
-    def mknod(self, path, mode, dev):
-        """Creates a new file
-        dev can be safely ignored
-        """
-        if self.debug == True:
-            logging.debug("In mknod, path = %s" % path)
-        # Create the file in the database
-        pass
-    '''
-
-    '''
     def mkdir(self, path, mode):
-        """Makes a new directory
-        """
-        if self.debug == True:
-            logging.debug("In mkdir, path = %s" % path)
-    '''
+        self.files[path] = dict(st_mode=(S_IFDIR | mode), st_nlink=2,
+                                st_size=0, st_ctime=time(), st_mtime=time(),
+                                st_atime=time())
 
-    '''
-    def utimens(self, path, times=None):
-        """Changes the modification/access time of a file
-        path = path to file
-        atime = access time
-        mtime = modification time
-        """
-        if self.debug == True:
-            logging.debug("In utimens, path = %s" % path)
-    '''
+        self.files['/']['st_nlink'] += 1
 
-    '''
-    def access(self, path, mode):
-        """Checks if a file can be accessed by the user
-        Not an issue for us
-        Will not be implemented
-        """
-        if self.debug == True:
-            logging.debug("In access, path = %s" % path)
-        pass
-    '''
+    def open(self, path, flags):
+        self.fd += 1
+        return self.fd
 
-    '''
+    def read(self, path, size, offset, fh):
+        return self.data[path][offset:offset + size]
+
+    def readdir(self, path, fh):
+        return ['.', '..'] + [x[1:] for x in self.files if x != '/']
+
+    def readlink(self, path):
+        return self.data[path]
+
+    def removexattr(self, path, name):
+        attrs = self.files[path].get('attrs', {})
+
+        try:
+            del attrs[name]
+        except KeyError:
+            pass        # Should return ENOATTR
+
+    def rename(self, old, new):
+        self.files[new] = self.files.pop(old)
+
+    def rmdir(self, path):
+        self.files.pop(path)
+        self.files['/']['st_nlink'] -= 1
+
+    def setxattr(self, path, name, value, options, position=0):
+        # Ignore options
+        attrs = self.files[path].setdefault('attrs', {})
+        attrs[name] = value
+
     def statfs(self, path):
-        """Return filesystem information
-        Since this isn't a real filesystem we really don't have anything valid to return
-        Will not be implemented
-        """
-        if self.debug == True:
-            logging.debug("In statfs, path = %s" % path)
-        pass
-    '''
+        return dict(f_bsize=512, f_blocks=4096, f_bavail=2048)
 
-    '''
-    def fsinit(self):
-        """Called when the filesystem is ready to serve requests.
-        Often used for spawning threads and such
-        Will need to be implemented to:
-            Calculate current local file hashes
-            Commit/update them to the newest version available
-        """
-        pass
-    '''
+    def symlink(self, target, source):
+        self.files[target] = dict(st_mode=(S_IFLNK | 0777), st_nlink=1,
+                                  st_size=len(source))
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Links a folder to the cloud-ish dvfs filesystem")
-    parser.add_argument("base", help="The folder to store local file copies in")
-# This argument needs to be last and is actually handled by the fuse module later
-    parser.add_argument("target", help="The folder to access the filesystem through")
-    parser.add_argument("-d", "--debug", action="store_true", help="Activates debug mode")
-    args = parser.parse_args()
-    if args.debug == True:
-        logging.basicConfig(filename='debug.log', level=logging.DEBUG)
+        self.data[target] = source
 
-    fuse = FUSE(dvfs(args.base, args.debug), args.target, foreground=False, nothreads=True)
+    def truncate(self, path, length, fh=None):
+        self.data[path] = self.data[path][:length]
+        self.files[path]['st_size'] = length
+
+    def unlink(self, path):
+        self.files.pop(path)
+
+    def utimens(self, path, times=None):
+        now = time()
+        atime, mtime = times if times else (now, now)
+        self.files[path]['st_atime'] = atime
+        self.files[path]['st_mtime'] = mtime
+
+    def write(self, path, data, offset, fh):
+        self.data[path] = self.data[path][:offset] + data
+        self.files[path]['st_size'] = len(self.data[path])
+        return len(data)
+
+
+if __name__ == '__main__':
+    if len(argv) != 2:
+        print('usage: %s <mountpoint>' % argv[0])
+        exit(1)
+
+    logging.getLogger().setLevel(logging.DEBUG)
+    fuse = FUSE(Memory(), argv[1], foreground=True)
---
+++ dvfs/oldDvfs.py
@@ -0,0 +1,286 @@
+#!/usr/bin/env python
+from fuse import FUSE, FuseOSError, Operations, LoggingMixIn
+import fuse
+from stat import S_IFDIR, S_IFLNK, S_IFREG
+import argparse #For easy parsing of the command line arguments
+import logging #For debug/error logging
+import couchdbkit as ck
+import os
+from datetime import datetime
+from couchdb.dbObject import dbObject
+from couchdb.dbFile import dbFile
+from couchdb.dbFolder import dbFolder
+
+
+""" If we were using the old python fuse bindings
+class rootInfo(fuse.Stat):
+    def __init__(self):
+        fuse.Stat.__init__(self)
+        self.st_mode = stat.S_IFDIR | 0755
+        self.st_nlink = 2
+
+class folderInfo(fuse.Stat):
+    def __init__(self):
+        fuse.Stat.__init__(self)
+        self.st_mode = stat.S_IFDIR | 0755
+        self.st_nlink = 2
+
+class fileInfo(fuse.Stat):
+    def __init__(self):
+        fuse.Stat.__init__(self)
+        self.st_mode = stat.S_IFREG | 0644
+        self.st_nlink = 1
+        self.st_size = 4096
+"""
+
+class dvfs(LoggingMixIn, Operations):
+    def __init__(self, base, debug):
+        self.debug = debug
+        if self.debug == True:
+            logging.debug("init, base = %s" % base)
+        if base[0] == '/':
+            self.base = base
+        else:
+            self.base = os.path.dirname(os.path.realpath(__file__)) + "/" + base
+        if self.debug == True:
+            logging.debug("init done, self.base = %s" % self.base)
+        self.connectDatabase(dbName='dvfs')
+
+    def connectDatabase(self,dbName):
+        """Sets up the database to be interacted"""
+        if self.debug == True:
+            logging.debug("connecting to database")
+        server = ck.Server()
+        self.database = server.get_or_create_db(dbName)
+        self.dataOb = dbObject.set_db(server.get_or_create_db(dbName))
+
+    def getattr(self, path, fh=None):
+        """Returns the attributes of the specific file"""
+        if self.debug == True:
+            logging.debug("Inside the getattr function, path= %s" % path)
+
+        # All of this is debug stuff
+        from time import time
+        import errno
+        now = time()
+        st = fuse.c_stat()
+        logging.debug(st)
+        return st
+        """
+        if path == '/':
+            return dict(st_mode=(S_IFDIR | 0755), st_ctime=now, st_mtime=now, st_atime=now, st_nlink=2)
+        return dict(st_mode=(S_IFDIR | 0444), st_ctime=now, st_mtime=now, st_atime=now, st_size=4096)
+        """
+        """
+        st.st_mode = stat.S_IFDIR | 0755
+        st.st_nlink = 2
+        st.st_atime = int(time.time())
+        st.st_mtime = st.st_atime
+        st.st_ctime = st.st_atime
+        if path != '/':
+            st.st_mode = stat.S_IFDIR | 0644
+            st.st_nlink = 1
+            st.st_size = 4096
+        return st
+        """
+        # End debug stuff
+        """
+        if path == '/':
+            logging.debug("found the root")
+            now = datetime.now()
+            return dict(st_mode=(S_IFDIR | 0755), st_ctime=now, st_mtime=now, st_atime=now, st_nlink=2)
+        logging.debug("outside the root")
+        return dict(st_mode=(S_IFDIR | 0755), st_ctime=now, st_mtime=now, st_atime=now)
+        dbView = dbFile(self.dataOb)
+        fileInfo = dbView.view('dvfs/path-all', key=path).one() #There should only ever be one anyway
+        if fileInfo is None:
+            raise FuseOSError(ENOENT)
+        else:
+            return fileInfo.getAttributes()
+        """
+
+    def readdir(self, path, fh=None):
+        """Returns the attributes of a folder"""
+        if self.debug == True:
+            logging.debug("Inside the readdir function, path= %s" % path)
+        return fuse.c_stat()
+        return dict(st_mode=(S_IFDIR | 0755), st_ctime=now, st_mtime=now, st_atime=now, st_nlink=2)
+        direntry = ['.', '..'] 
+        dbView = dbFile(self.dataOb)
+        fileList = dbView.view('dvfs/folderPath', key=path)
+        for entry in fileList:
+            direntry.append(entry.path[len(path):].encode('utf-8')) #A bit ugly, but will always work by definition
+        return direntry
+
+    def read(self, path, size, offset, fh=None):
+        """Loads data from an opened file"""
+        if self.debug == True:
+            logging.debug("the read path is= " + path)
+        return ''
+
+    def open(self, path, flags=None):
+        """For checking permission of pending file
+        Needs to be implemented to pull down files that we don't have"""
+        if self.debug == True:
+            logging.debug("the open path is= " + path)
+        return -errno.EACCES
+
+    def write(self, path, data, offset, fh=None):
+        #Update the database metadata
+        """Actually writes data into the file"""
+        if self.debug == True:
+            logging.debug("In the write call")
+        return 0
+
+# These features aren't implemented yet, and some may never be
+    '''
+    def readlink(self, path):
+        """Reads a symbolic link
+        May be implemented
+        """
+        if self.debug == True:
+            logging.debug("In readlink, path= %s" % path)
+        pass
+    '''
+
+    '''
+    def unlink(self, path):
+        """Removes a file"""
+        if self.debug == True:
+            logging.debug("In unlink, path = %s" % path)
+        pass
+    '''
+
+    '''
+    def rmdir(self, path):
+        """Removes a directory
+        Will need to be implemented
+        """
+        if self.debug == True:
+            logging.debug("In rmdir, path = %s" % path)
+        pass
+    '''
+
+    '''
+    def symlink(self, path, path1):
+        """Creates a symbolic path
+        May be implemented
+        """
+        if self.debug == True:
+            logging.debug("In symlink, path = %s" % path)
+        pass
+    '''
+
+    '''
+    def link(self, path, path1):
+        """Creates a hard link
+        May be implemented
+        """
+        if self.debug == True:
+            logging.debug("In link, path = %s" % path)
+        pass
+    '''
+
+    '''
+    def chmod(self, path, mode):
+        """Changes the permissions on a file
+        Will need to be implemented, and will be complex
+        """
+        if self.debug == True:
+            logging.debug("In chmod, path = %s" % path)
+        return 0
+    '''
+
+    '''
+    def chown(self, path, mode):
+        """Changes the owner/group of a file
+        Will need to be implemented
+        """
+        if self.debug == True:
+            logging.debug("In chown, path = %s" % path)
+    '''
+
+    '''
+    def truncate(self, path, length, fh=None):
+        """Changes the size of a file
+        Should be easy to implement
+        """
+        if self.debug == True:
+            logging.debug("In truncate, path = %s" % path)
+        pass
+    '''
+
+    '''
+    def mknod(self, path, mode, dev):
+        """Creates a new file
+        dev can be safely ignored
+        """
+        if self.debug == True:
+            logging.debug("In mknod, path = %s" % path)
+        # Create the file in the database
+        pass
+    '''
+
+    '''
+    def mkdir(self, path, mode):
+        """Makes a new directory
+        """
+        if self.debug == True:
+            logging.debug("In mkdir, path = %s" % path)
+    '''
+
+    '''
+    def utimens(self, path, times=None):
+        """Changes the modification/access time of a file
+        path = path to file
+        atime = access time
+        mtime = modification time
+        """
+        if self.debug == True:
+            logging.debug("In utimens, path = %s" % path)
+    '''
+
+    '''
+    def access(self, path, mode):
+        """Checks if a file can be accessed by the user
+        Not an issue for us
+        Will not be implemented
+        """
+        if self.debug == True:
+            logging.debug("In access, path = %s" % path)
+        pass
+    '''
+
+    '''
+    def statfs(self, path):
+        """Return filesystem information
+        Since this isn't a real filesystem we really don't have anything valid to return
+        Will not be implemented
+        """
+        if self.debug == True:
+            logging.debug("In statfs, path = %s" % path)
+        pass
+    '''
+
+    '''
+    def fsinit(self):
+        """Called when the filesystem is ready to serve requests.
+        Often used for spawning threads and such
+        Will need to be implemented to:
+            Calculate current local file hashes
+            Commit/update them to the newest version available
+        """
+        pass
+    '''
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Links a folder to the cloud-ish dvfs filesystem")
+    parser.add_argument("base", help="The folder to store local file copies in")
+# This argument needs to be last and is actually handled by the fuse module later
+    parser.add_argument("target", help="The folder to access the filesystem through")
+    parser.add_argument("-d", "--debug", action="store_true", help="Activates debug mode")
+    args = parser.parse_args()
+    if args.debug == True:
+        logging.basicConfig(filename='debug.log', level=logging.DEBUG)
+
+    fuse = FUSE(dvfs(args.base, args.debug), args.target, foreground=False, nothreads=True)
